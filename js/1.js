c10f17da4d74ab4e10bf356a555175517b98a1f1
Ciel丶Samuel <30369468+a4410505@users.noreply.github.com>
2017-08-04 14:21:06 +0800

    1

--- js/1.js
+++ js/1.js
@@ -1,21 +1,31 @@
-var x = 0;
-var y = 0;
-function t1(){
-	x ++;
+$(document).ready(function(){
+  $(".loading").css("display","block");
+  $(".expand").animate({width:"100%"},5000,function(){
+  	$(".loading").css("display","none");
+  });
+});
+
+function rota(){
+var x= 0;
+var y= 0;
+$("#Arrow_up").bind("click",function(){
+	x++;
 	$("#C1").css({"transform":"rotateX("+x*90+"deg)","transition":"1s"});
-};
+});
 
-function t2(){
-	$("#C1").css({"transform":"rotateX("+-x*90+"deg)","transition":"1s"});
-	x ++;
-};
+$("#Arrow_down").bind("click",function(){
+	--x;
+	$("#C1").css({"transform":"rotateX("+x*90+"deg)","transition":"1s"});
+});
 
-function t3(){
-	y ++;
+$("#Arrow_left").bind("click",function(){
+	y++;
 	$("#C1").css({"transform":"rotateY("+y*90+"deg)","transition":"1s"});
-};
+});
 
-function t4(){
-	y ++;
-	$("#C1").css({"transform":"rotateY(-"+y*90+"deg)","transition":"1s"});
+$("#Arrow_right").bind("click",function(){
+	--y;
+	$("#C1").css({"transform":"rotateY("+y*90+"deg)","transition":"1s"});
+});
 };
+rota();
